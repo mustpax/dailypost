ca881ad5819466b5c980226667dc9d9121257f67
Mustafa Paksoy <paksoy@gmail.com>
2008-09-04 00:02:25 -0400

    Adding Python install capability using distutils.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!env python
+
+from distutils.core import setup
+
+setup(name="dailypost",
+      version="0.2",
+      description="Manage MOTD entries and keep your MOTD looking fine.",
+      author="Mustafa Paksoy",
+      url="https://github.com/mustpax/dailypost",
+      py_modules=["config"],
+      scripts=["dailypost.py"])
+
